73491ba25c88bae25d0653c31344b61b64106054
Deme <deme@debian.es>
2018-09-03 13:57:42 +0200

    Added eslint support

---
+++ tests/src/index.js
@@ -0,0 +1,18 @@
+// Copyright 1-Sep-2017 ºDeme
+// GNU General Public License - V3 <http://www.gnu.org/licenses/>
+
+import B64Test from "./B64Test.js";
+import CrypTest from "./CrypTest.js";
+import DecTest from "./DecTest.js";
+import TpTest from "./TpTest.js";
+import RndTest from "./RndTest.js";
+import ItTest from "./ItTest.js";
+
+export function main () {
+  B64Test.run();
+  CrypTest.run();
+  DecTest.run();
+  TpTest.run();
+  RndTest.run();
+  ItTest.run();
+}
