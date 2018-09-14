f8703d9573777187ba7e8762a9f63753d406ca08
Deme <deme@debian.es>
2018-09-14 20:18:05 +0200

    Adapted Client to Promise's

--- src/dmjs/Client.js
+++ src/dmjs/Client.js
@@ -1,4 +1,4 @@
-// Copyright 09-Sep-2017 ºDeme
+// Copyright 14-Sep-2018 ºDeme
 // GNU General Public License - V3 <http://www.gnu.org/licenses/>
 
 /**
@@ -23,9 +23,9 @@
  *     userKey -> in Cryp.key.
  *     [0/1] -> "1" Indicates temporary connection. "0" Indicates permanent.
  *   CONNECT: sessionId
- *     sessionId -> As came from server.
+ *     sessionId -> As it cames from server.
  *   NORMAL COMMUNICATION: sessionId:data
- *     sessionId -> As came from server.
+ *     sessionId -> As it cames from server.
  *     data -> A pageId field is added. After that it is codified con
  *             Cryp.cryp and communicationKey
  *
@@ -143,170 +143,168 @@ export default class Client {
    * @private
    * @param {boolean} asynch If connection is asynchronic.
    * @param {string} rq data to send in B64
-   * @param {function (string):void} f Action to do. The string of 'f' is
-   *        B64 codified.
-   * @return {void}
+   * @return {!Promise}
    */
-  sendServer (asynch, rq, f) {
+  sendServer (asynch, rq) {
     const self = this;
+    return new Promise(function (resolve, reject) {
+      const request = new XMLHttpRequest();
 
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = () => {
-      if (request.readyState === 4) {
-        self._lock = false;
-        f(request.responseText.trim());
+      request.onload = () => {
+        if (request.status === 200) {
+          self._lock = false;
+          resolve(request.responseText.trim());
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+
+      request.onerror = () => {
+        reject(Error("Network Error"));
+      };
+
+      if (self._lock && !asynch) {
+        return;
       }
-    };
-    if (self._lock && !asynch) {
-      return;
-    }
-    self._lock = true;
-    request.open(
-      "POST",
-      "http://" + location.host + "/cgi-bin/ccgi.sh",
-      true
-    );
-    request.setRequestHeader(
-      "Content-Type"
-      , "text/plain"
-    );
-    request.send(this._appName + ":" + rq);
+      self._lock = true;
+
+      request.open(
+        "POST",
+        "http://" + location.host + "/cgi-bin/ccgi.sh",
+        true
+      );
+      request.setRequestHeader(
+        "Content-Type"
+        , "text/plain"
+      );
+
+      request.send(self._appName + ":" + rq);
+    });
   }
 
   /**
-   * @param {function (boolean):void} f 'f' receives ok
-   * @return {void}
+   * @return {!Promise}
    */
-  connect (f) {
+  async connect () {
     const self = this;
-    self.sendServer(
-      false,
-      self.sessionId(),
-      rp => {
-        try {
-          const jdata = Cryp.decryp(self.sessionId(), rp);
-          const data = /** @type {!Object<string, ?>} */(JSON.parse(jdata));
-          const key = data["key"];
-          if (key === "") {
-            f(false);
-          } else {
-            self.setKey(key);
-            self._connectionId = data["connectionId"];
-            f(true);
-          }
-        } catch (e) {
-          //eslint-disable-next-line
-          console.log(`RAW SERVER RESPONSE:\n${rp}\nCLIENT ERROR:\n${e}`);
-        }
+    const rp = await self.sendServer(false, self.sessionId());
+    try {
+      const jdata = Cryp.decryp(self.sessionId(), rp);
+      const data = /** @type {!Object<string, ?>} */(JSON.parse(jdata));
+      const key = data["key"];
+      if (key === "") {
+        return false;
       }
-    );
+      self.setKey(key);
+      self._connectionId = data["connectionId"];
+      return true;
+    } catch (e) {
+      //eslint-disable-next-line
+      console.log(`RAW SERVER RESPONSE:\n${rp}\nCLIENT ERROR:\n${e}`);
+      return null;
+    }
   }
 
   /**
    * @param {string} user Param
    * @param {string} pass (As is written for user)
    * @param {boolean} expiration "true" means a temporary connection.
-   * @param {function (boolean):void} f Function
-   * @return {void}
+   * @return {!Promise}
    */
-  authentication (user, pass, expiration, f) {
+  async authentication (user, pass, expiration) {
     const self = this;
     const key = Cryp.key(self._appName, Klen);
     const p = Client.crypPass(pass);
     const exp = expiration ? "1" : "0";
-    self.sendServer(
+    const rp = await self.sendServer(
       false,
-      ":" + Cryp.cryp(key, `${user}:${p}:${exp}`),
-      rp => {
-        try {
-          const jdata = Cryp.decryp(key, rp);
-          const data = /** @type {!Object<string, ?>} */(JSON.parse(jdata));
-          const sessionId = data["sessionId"];
-          if (sessionId === "") {
-            f(false);
-          } else {
-            self.setKey(data["key"]);
-            self.setSessionId(sessionId);
-            Store.put("Client_user_" + self._appName, user);
-            f(true);
-          }
-        } catch (e) {
-          //eslint-disable-next-line
-          console.log(`RAW SERVER RESPONSE:\n${rp}\nCLIENT ERROR:\n${e}`);
-        }
-      }
+      ":" + Cryp.cryp(key, `${user}:${p}:${exp}`)
     );
+    try {
+      const jdata = Cryp.decryp(key, rp);
+      const data = /** @type {!Object<string, ?>} */(JSON.parse(jdata));
+      const sessionId = data["sessionId"];
+      if (sessionId === "") {
+        return false;
+      }
+      self.setKey(data["key"]);
+      self.setSessionId(sessionId);
+      Store.put("Client_user_" + self._appName, user);
+      return true;
+    } catch (e) {
+      //eslint-disable-next-line
+      console.log(`RAW SERVER RESPONSE:\n${rp}\nCLIENT ERROR:\n${e}`);
+      return null;
+    }
   }
 
   /**
    * @private
    * @param {!Object<string, ?>} data Param
-   * @param {function (!Object<string, ?>):void} f Function
    * @param {boolean} asynch If connection is asynchronic.
    * @param {boolean} withConnectionId Param
-   * @return {void}
+   * @return {!Promise}
    */
-  _send (data, f, asynch, withConnectionId) {
+  async _send (data, asynch, withConnectionId) {
     const self = this;
     if (withConnectionId) {
       data["connectionId"] = self._connectionId;
     }
-    self.sendServer(
+    const rp = await self.sendServer(
       asynch,
-      self.sessionId() + ":" + Cryp.cryp(self.key(), JSON.stringify(data)),
-      rp => {
-        try {
-          const jdata = Cryp.decryp(self.key(), rp);
-          const data = /** @type {!Object<string, ?>} */(JSON.parse(jdata));
-          f(data);
-        } catch (e) {
-          try {
-            const jdata = Cryp.decryp("nosession", rp);
-            const data = /** @type {!Object<string, ?>} */(JSON.parse(jdata));
-            const expired = data["expired"] || false;
-            if (expired) {
-              self._fexpired();
-            } else {
-              throw(e);
-            }
-          } catch (e2) {
-            //eslint-disable-next-line
-            console.log(`RAW SERVER RESPONSE:\n${rp}\nCLIENT ERROR:\n${e}`);
-          }
-        }
-      }
+      self.sessionId() + ":" + Cryp.cryp(self.key(), JSON.stringify(data))
     );
+
+    try {
+      const jdata = Cryp.decryp(self.key(), rp);
+      const data = /** @type {!Object<string, ?>} */(JSON.parse(jdata));
+      return data;
+    } catch (e) {
+      try {
+        const jdata = Cryp.decryp("nosession", rp);
+        const data = /** @type {!Object<string, ?>} */(JSON.parse(jdata));
+        const expired = data["expired"] || false;
+        if (expired) {
+          self._fexpired();
+          return null;
+        }
+        throw(e);
+      } catch (e2) {
+        //eslint-disable-next-line
+        console.log(`RAW SERVER RESPONSE:\n${rp}\nCLIENT ERROR:\n${e}`);
+        return null;
+      }
+    }
   }
 
   /**
-   * [send0] does not check if connectionId is correct.
+   * [send0] does not check if connectionId is correct. If there is other
+   * connection in course, the call is discarted.
    * @param {!Object<string, ?>} data Param
-   * @param {function (!Object<string, ?>):void} f Function
-   * @return {void}
+   * @return {!Promise}
    */
-  send0 (data, f) {
-    this._send(data, f, false, false);
+  send0 (data) {
+    return this._send(data, false, false);
   }
 
   /**
-   * [send] checks if connectionId is correct.
+   * [send] checks if connectionId is correct. If there is other connection
+   * in course, the call is discarted.
    * @param {!Object<string, ?>} data Param
-   * @param {function (!Object<string, ?>):void} f Function
-   * @return {void}
+   * @return {!Promise}
    */
-  send (data, f) {
-    this._send(data, f, false, true);
+  send (data) {
+    return this._send(data, false, true);
   }
 
   /**
    * [sendAsync] sends asynchronically 'data' and does not check if
    * connectionId is correct.
    * @param {!Object<string, ?>} data Param
-   * @param {function (!Object<string, ?>):void} f Function
-   * @return {void}
+   * @return {!Promise}
    */
-  sendAsync (data, f) {
-    this._send(data, f, true, false);
+  sendAsync (data) {
+    return this._send(data, true, false);
   }
 
   /**
